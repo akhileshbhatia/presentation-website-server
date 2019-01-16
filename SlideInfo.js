2089e76e4d061d7f156f7c12571f08f152f380cf
Akhilesh Bhatia <akhileshbhatia93@gmail.com>
2019-01-16 22:42:46 +0000

    changed SlideInfo class implementation

--- SlideInfo.js
+++ SlideInfo.js
@@ -1,23 +1,26 @@
 const path = require("path");
 const fs = require("fs");
 const prop = require("./property-loader");
-
+const os = require("os");
 class SlideInfo {
     constructor(slideNum) {
         this.slideNum = slideNum;
-        this.isFirstSlide = +slideNum === 1 ? true : false;
-        this.imageFilePath = this.imagePath;
-        this.audioFilePath = this.audioPath;
-        this.textData = this.textFromFile;
-        this.isLastSlide = +slideNum === prop.totalSlides ? true : false;
+    }
+
+    get isFirstSlide() {
+        return this.slideNum === 1 ? true : false;
+    }
+
+    get isLastSlide() {
+        return this.slideNum === prop.totalSlides ? true : false;
     }
     get imagePath() {
-        return path.join(__dirname, prop.presentationRoot, prop.imageFolder,
-            (prop.imageFilePrefix + " " + this.slideNum + prop.imageFileType));
+        return path.join(prop.presentationRoot, prop.imageFolder,
+            (prop.imageFilePrefix + " " + (this.slideNum + prop.imageFileType)));
     }
 
     get audioPath() {
-        return path.join(__dirname, prop.presentationRoot, prop.audioFolder,
+        return path.join(prop.presentationRoot, prop.audioFolder,
             (prop.audioFilePrefix + " " + this.slideNum + prop.audioFileType));
     }
 
@@ -31,6 +34,18 @@ class SlideInfo {
         catch (err) {
             return ("Error in reading file " + err);
         }
+    }
+
+    get SlideResponse() {
+        let slideResponse = {};
+        slideResponse["slideNum"] = this.slideNum;
+        slideResponse["isFirstSlide"] = this.isFirstSlide;
+        slideResponse["isLastSlide"] = this.isLastSlide;
+        slideResponse["imagePath"] = this.imagePath;
+        slideResponse["audioPath"] = this.audioPath;
+        slideResponse["text"] = this.textFromFile;
+
+        return slideResponse;
     }
 }
 
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
 
 const allowCrossDomain = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    console.log("Request received");
+    // console.log("Request received");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Cache-Control", "no-cache");
@@ -15,32 +17,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(allowCrossDomain);
 
+
 const prop = require("./property-loader");
 const SlideInfo = require("./SlideInfo");
 
+app.use(express.static(prop.presentationRoot));
 
 isValidSlideNum = (slideNum) => {
     let isValid = false;
     if (slideNum != undefined) {
-        let slideNumInt = +slideNum;
         //1st expression i.e. slideNumInt != 0 returns false if slideNum contains only spaces or empty string
         //2nd is the regExp to check if its a valid integer
         //3rd is to verify if the slide number is within the range of available slides
         //4th and 5th verify if the slide is within valid range of slide numbers
-        isValid = (slideNumInt != 0) && (/^\d+$/.test(slideNumInt)) && (slideNumInt >= 1)
-            && (slideNumInt <= prop.totalSlides);
+        isValid = (slideNum != 0) && (/^\d+$/.test(slideNum)) && (slideNum >= 1)
+            && (slideNum <= prop.totalSlides);
     }
 
     return isValid;
 }
 
 app.get("/api/slide/:id", (req, res) => {
+    // console.log("request received");
+    let slideNum = +req.params.id;
     let response = {};
-    if (isValidSlideNum(req.params.id))
-        response = new SlideInfo(req.params.id);
-    else
-        response["err"] = "Incorrect request";
-    res.send(response);
+    if (isValidSlideNum(slideNum)) {
+        let slideInfo = new SlideInfo(slideNum);
+        response = slideInfo.SlideResponse;
+        res.send(response);
+        // console.log(response);
+    }
+    else {
+        response["err"] = "error in response";
+        res.send(response);
+    }
+
 })
 
 app.listen(prop.port, (err) => {
